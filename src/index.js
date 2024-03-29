ca29953beb3245e1983f482f9747dfbcfecd8182
BigFatDuck1 <clementho999@gmail.com>
2024-03-29 22:40:51 +0800

    Added support for importing photos through file loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import "./style.css"
+import "./style.css";
+import Photo from "./photo.png";
+
+
+const myPhoto = new Image();
+myPhoto.src = Photo;
+document.querySelector("#photo").src = myPhoto.src;
