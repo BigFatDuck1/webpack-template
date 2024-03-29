7125c5395ad2bb333c244405e627b1e893048a90
BigFatDuck1 <clementho999@gmail.com>
2024-03-29 21:32:00 +0800

    Changed webpack.config.js to use template html file in src folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,16 @@ const path = require('path');
 
 module.exports = {
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(
+        {
+            hash: true, //Cache busting
+            title: "Title",
+            header: "Header",
+            metaDesc: "Description",
+            template: "./src/index.html", //This line tells webpack to use a template html instead of generating its own and putting it into /dist
+            filename: "./index.html", //Filename for the output file in /dist
+            inject: "body", //Inject js script in <body> instead of <head>
+        }
+    )
   ]
 };
